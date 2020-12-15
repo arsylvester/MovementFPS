6eca891727c105ab3ad508218619a87c93a5bbdc
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-12-15 13:24:03 -0600

    Unity Input System added and files rearranged

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(horizontal, 0, vertical);
+    }
+
+   // private void MoveHorizontalInput(InputAction)
+}
