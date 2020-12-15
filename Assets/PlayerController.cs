0d5db689116013c676480e8ca262165b8cc2a6e7
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-12-15 13:52:54 -0600

    WASD Move with new input system

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 1;
+    float horizontal;
+    float vertical;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.Translate(horizontal, 0, vertical);
     }
 
-   // private void MoveHorizontalInput(InputAction)
+   public void OnMove(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x * speed * .1f;
+        vertical = context.ReadValue<Vector2>().y * speed * .1f;
+        Debug.Log(context.ReadValue<Vector2>());
+    }
 }
