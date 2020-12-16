d2f57b80e4c2ce2ff85c7d0a41f2b04939983c29
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-12-15 19:56:46 -0600

    Collisions using playercontroller, reworked movement with camera angle

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,50 +9,55 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float mouseSensitivity = 1;
     [SerializeField] float lookVerticalMin = -85;
     [SerializeField] float lookVerticalMax = 85;
-    float horizontal;
-    float vertical;
+    Vector2 inputVector;
+    Vector3 movementVector;
     Vector2 cameraMovement;
     PlayerInput playerInput;
+    CharacterController characterController;
 
-    // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(horizontal, 0, vertical);
+        //Using theorem x2 = xcosB - ysinB
+        //y2 = xsinB + ycosB
+        //Unity works clockwise, math typically goes counterclockwise so use recipical? (360 - angle)
+        float directionAngle = (360 - transform.eulerAngles.y) * Mathf.Deg2Rad;
+        movementVector.x = ((Mathf.Cos(directionAngle) * inputVector.x) - (Mathf.Sin(directionAngle) * inputVector.y)) * speed;
+        movementVector.z = ((Mathf.Sin(directionAngle) * inputVector.x) + (Mathf.Cos(directionAngle) * inputVector.y)) * speed;
+        characterController.Move(movementVector);
+        //Debug.Log("Angle: " + directionAngle * Mathf.Rad2Deg + " x: " + movementVector.x + " z: " + movementVector.z + " Input x:" + inputVector.x + " Input y:" + inputVector.y);
     }
 
    public void OnMove(InputAction.CallbackContext context)
     {
-        horizontal = context.ReadValue<Vector2>().x * speed;
-        vertical = context.ReadValue<Vector2>().y * speed;
-        //playerInput.camera.transform.eulerAngles.
-        //Debug.Log(context.ReadValue<Vector2>());
+        inputVector = context.ReadValue<Vector2>();
     }
 
     public void OnLook(InputAction.CallbackContext context)
     {
+        //Was using camera rotation, but that had some stutter issues. Rotating whole player seems to work. If issues down the line might have to revert back to adjusting camera.
         cameraMovement = context.ReadValue<Vector2>() * mouseSensitivity;
         Vector3 currentRotation = playerInput.camera.transform.localEulerAngles;
 
         //Add last frame angle with mousedelta and clamp the vertical. Mathf.clamp won't work with the angles so have to custom clamp it.
-        float xRotation = (cameraMovement.y * mouseSensitivity * -1) + currentRotation.x;
-        if(currentRotation.x < 90 && xRotation > lookVerticalMin)
+        float xRotation = (cameraMovement.y * mouseSensitivity * -1) + transform.localEulerAngles.x;
+        if(transform.localEulerAngles.x < 90 && xRotation > lookVerticalMin)
         {
             xRotation = lookVerticalMin;
         }
-        else if(currentRotation.x > 270 && xRotation < lookVerticalMax)
+        else if(transform.localEulerAngles.x > 270 && xRotation < lookVerticalMax)
         {
             xRotation = lookVerticalMax;
         }
         float yRotation = (cameraMovement.x * mouseSensitivity) + transform.localEulerAngles.y;
 
         //Rotate camera's x and player's y
-        playerInput.camera.transform.localEulerAngles = new Vector3(xRotation, 0, 0);
-        transform.localEulerAngles = new Vector3(0, yRotation, 0);
+       // playerInput.camera.transform.localEulerAngles = new Vector3(xRotation, 0, 0);
+        transform.localEulerAngles = new Vector3(xRotation, yRotation, 0);
     }
 }
