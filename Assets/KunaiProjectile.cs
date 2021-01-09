c8c602920d826ed4d2c876fbfe256f9ac28140f5
Andrew Sylvester <andrewrsylvester@gmail.com>
2021-01-09 17:58:56 -0600

    Kunai now uses raycasts to detect collisions

--- Assets/KunaiProjectile.cs
+++ Assets/KunaiProjectile.cs
@@ -7,6 +7,10 @@ public class KunaiProjectile : MonoBehaviour
     private float throwSpeed;
     private bool shouldMove = true;
     private int damageToDeal;
+    private Vector3 lastPoint;
+    private Vector3 lastPointDirection;
+    private RaycastHit hit;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,20 @@ public class KunaiProjectile : MonoBehaviour
         if (shouldMove)
         {
             transform.Translate(transform.forward * throwSpeed * Time.deltaTime, Space.World);
+            lastPointDirection = transform.position - lastPoint;
+            Debug.DrawRay(transform.position, lastPointDirection, Color.cyan, Vector3.Distance(transform.position, lastPoint));
+            if(Physics.Raycast(transform.position, lastPointDirection, out hit, Vector3.Distance(transform.position, lastPoint)))
+            {
+                if (hit.transform.tag != "Player")
+                {
+                    shouldMove = false;
+                    if (hit.transform.GetComponent<IDamageable>() != null)
+                    {
+                        hit.transform.GetComponent<IDamageable>().TakeDamage(damageToDeal);
+                    }
+                }
+            }
+            lastPoint = transform.position;
         }
     }
 
@@ -27,18 +45,7 @@ public class KunaiProjectile : MonoBehaviour
     {
         throwSpeed = speed;
         damageToDeal = damage;
+        lastPoint = transform.position;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        print("Hit: " + other);
-        if(other.tag != "Player")
-        {
-            shouldMove = false;
-            if(other.GetComponent<IDamageable>() != null)
-            {
-                other.GetComponent<IDamageable>().TakeDamage(damageToDeal);
-            }
-        }
-    }
 }
