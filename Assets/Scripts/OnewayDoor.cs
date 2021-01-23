201cc515f530f701f253fdd2d98d25a6ed285948
Andrew Sylvester <andrewrsylvester@gmail.com>
2021-01-22 18:21:26 -0600

    Starting area finished and oneway door

---
+++ Assets/Scripts/OnewayDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnewayDoor : MonoBehaviour
+{
+    [SerializeField] bool enterence;
+    [SerializeField] Collider oneWayCollider; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerController>())
+        {
+            if (enterence)
+            {
+                oneWayCollider.enabled = false;
+            }
+            else
+            {
+                oneWayCollider.enabled = true;
+            }
+        }
+    }
+}
