bf6338d52d72b8c7ddbf38faf3feb32a525f97d8
Andrew Sylvester <andrewrsylvester@gmail.com>
2021-01-28 20:19:05 -0600

    MainMenu Structured out

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject menu;
+    [SerializeReference] GameObject options;
+
+    public void OnSceneStart(string sceneToMove)
+    {
+        SceneManager.LoadScene(sceneToMove);
+    }
+
+    public void ShowOptions()
+    {
+        menu.SetActive(false);
+        options.SetActive(true);
+    }
+    public void ReturnToMenu()
+    {
+        menu.SetActive(true);
+        options.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(0);
+    }
+}
