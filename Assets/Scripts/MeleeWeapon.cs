69ae7a308361459b86659557a21b3113276ebc44
Andrew Sylvester <andrewrsylvester@gmail.com>
2021-03-07 16:21:57 -0600

    Start of sword attack

---
+++ Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : Weapon
+{
+    //[SerializeField] protected bool hitScan;
+    [SerializeField] float meleeRange = 1;
+    [SerializeField] GameObject hitParticle;
+    [SerializeField] GameObject enemyHitParticle;
+
+    protected RaycastHit hit;
+    protected bool isHit;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override void UseWeapon()
+    {
+        if (coolDown + currentCoolDown < Time.time)
+        {
+            if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, meleeRange))
+            {
+                print("Melee hit : " + hit.transform);
+                isHit = true;
+                currentCoolDown = Time.time;
+                //Instantiate(objectAtEnd, hit.point, playerCamera.rotation);
+                if (hit.transform.GetComponent<IDamageable>() != null)
+                {
+                    hit.transform.GetComponent<IDamageable>().TakeDamage(damage);
+                    ui.ShowHitMarker();
+                    Instantiate(enemyHitParticle, hit.point, transform.rotation);
+                }
+                else
+                {
+                    Instantiate(hitParticle, hit.point, transform.rotation);
+                }
+            }
+            else
+            {
+                isHit = false;
+            }
+        }
+    }
+
+    public override void UseAltFireWeapon()
+    {
+
+    }
+
+    public override void AltFireWeaponRelease()
+    {
+        //StartCoroutine(Zoom(zoomAmount, oldZoom));
+    }
+
+}
